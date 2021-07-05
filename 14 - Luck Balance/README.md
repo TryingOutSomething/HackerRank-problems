7c741bdae750f24ea78f9fefd24ab90503869cff
TryingOutSomething <jan.owyeong123@gmail.com>
2021-07-05 22:20:02 +0800

    day 14

---
+++ 14 - Luck Balance/README.md
@@ -0,0 +1,1 @@
+# [Luck Balance](https://www.hackerrank.com/challenges/luck-balance/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=greedy-algorithms)
---
+++ 14 - Luck Balance/solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Result {
+
+    public static int luckBalance(int k, List<List<Integer>> contests) {
+        int totalLuck = 0;
+        List<Integer> importantMatchesLuckList = new ArrayList<>();
+
+        for (int i = 0; i < contests.size(); i++) {
+            List<Integer> contest = contests.get(i);
+
+            if (contest.get(1) == 1) {
+                importantMatchesLuckList.add(contest.get(0));
+            }
+
+            totalLuck += contest.get(0);
+        }
+
+        Collections.sort(importantMatchesLuckList);
+
+        if (importantMatchesLuckList.size() > k) {
+            for (int i = 0; i < importantMatchesLuckList.size() - k; i++) {
+                totalLuck -= 2 * importantMatchesLuckList.get(i);
+            }
+        }
+
+        return totalLuck;
+    }
+}
